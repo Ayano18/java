bddf81418871b1c2a1a95f242d4d04977cfd1665
Ayano18 <143322152+Ayano18@users.noreply.github.com>
2023-11-20 08:57:42 +0800

    Add files via upload

---
+++ Trip.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JOptionPane;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Timot
+ */
+public class Trip {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+        //while(true) {
+            //JOptionPane.showMessageDialog(null, "This is some useless info", "Notification", JOptionPane.PLAIN_MESSAGE);
+            //JOptionPane.showMessageDialog(null, "oke", "Notification", JOptionPane.INFORMATION_MESSAGE);
+            //JOptionPane.showMessageDialog(null, "ge po", "Notification", JOptionPane.QUESTION_MESSAGE);
+            //JOptionPane.showMessageDialog(null, "sge po", "Notification", JOptionPane.WARNING_MESSAGE);
+            //JOptionPane.showMessageDialog(null, "oumkay", "Notification", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showConfirmDialog(null, "uto uto kaba", "notify", JOptionPane.YES_NO_OPTION);
+            JOptionPane.showMessageDialog(null, "haha " + "tanga");
+        //}
+    }
+}
